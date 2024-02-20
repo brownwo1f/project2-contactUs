e8f7914b59ef431a55366fd6e6b33a0b53cd3a5b
Deepak Chauhan <deepak858783@gmail.com>
2024-02-21 04:29:23 +0530

    added some functionality, will add more as I will learn

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,27 +2,54 @@ import React from "react";
 import "./Form.css";
 
 const Form = () => {
+  const contact = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.target.className === "btns b1") {
+      console.log("Via Support Chat");
+    } else if (e.target.className === "btns b2") {
+      console.log("Via Call");
+    } else {
+      console.log("Via Email Form");
+    }
+  };
+
+  const submit = (e) => {
+    // e.preventDefault();
+    e.stopPropagation();
+    alert(
+      "SUCCESS! Your Concern has been submitted, we will reach out to you as soon as possible!"
+    );
+  };
   return (
     <div className="container">
-      <form>
-        <button className="btns b1">Via Support Chat</button>
-        <button className="btns b2">Via Call</button>
-        <button className="btns b3">Via Email From</button>
+      <button className="btns b1" onClick={contact}>
+        Via Support Chat
+      </button>
+      <button className="btns b2" onClick={contact}>
+        Via Call
+      </button>
+      <button className="btns b3" onClick={contact}>
+        Via Email From
+      </button>
+      <form onSubmit={submit}>
         <div className="entries">
           <fieldset>
             <legend>Name</legend>
-            <input></input>
+            <input type="text" required></input>
           </fieldset>
           <fieldset>
             <legend>E-mail </legend>
-            <input></input>
+            <input type="email" required></input>
           </fieldset>
           <fieldset>
             <legend>Text</legend>
-            <textarea></textarea>
+            <textarea type="text" required></textarea>
           </fieldset>
         </div>
-        <button className="btns b4">Submit</button>
+        <button type="submit" className="btns b4">
+          Submit
+        </button>
       </form>
     </div>
   );
